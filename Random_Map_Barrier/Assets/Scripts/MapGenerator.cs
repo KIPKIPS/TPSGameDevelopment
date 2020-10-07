65aa4b6a1f553ca81cf2576d39c11516607ec48d
KIPKIPS <2432719656@qq.com>
2020-10-07 23:59:38 +0800

    生成地图的算法完成

--- Random_Map_Barrier/Assets/Scripts/MapGenerator.cs
+++ Random_Map_Barrier/Assets/Scripts/MapGenerator.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour
-{
+public class MapGenerator : MonoBehaviour {
+    public GameObject tilePrefab;
+    public Vector2 mapSize;
+    public Transform mapHolder;
+    [Range(0, 1)] public float outlinePercent; //地图板块间隔
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        mapSize = new Vector2(8, 8);
+        outlinePercent = 0.1f;
+        tilePrefab = Resources.Load<GameObject>("Prefabs/Tile");
+        mapHolder = transform.Find("MapHolder").transform;
+        GenerateMap();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+
+    void GenerateMap() {
+        for (int i = 0; i < mapSize.x; i++) {
+            for (int j = 0; j < mapSize.y; j++) {
+                Vector3 newPos = new Vector3(-mapSize.x / 2 + 0.5f + i+outlinePercent*i, 0,-mapSize.y / 2 + 0.5f + j+ outlinePercent*j); //计算位置
+                GameObject tile = GameObject.Instantiate(tilePrefab, newPos, Quaternion.Euler(90,0,0), mapHolder);
+            }
+        }
     }
 }
