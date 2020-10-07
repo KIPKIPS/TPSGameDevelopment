db12070d2c0ace1f657f2bf456d07478bb8397bb
KIPKIPS <2432719656@qq.com>
2020-10-08 00:10:51 +0800

    修改生成地图的初始化数据

--- Random_Map_Barrier/Assets/Scripts/MapGenerator.cs
+++ Random_Map_Barrier/Assets/Scripts/MapGenerator.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
     public GameObject tilePrefab;
-    public Vector2 mapSize;
+    public Vector2 mapSize = new Vector2(8, 8);
     public Transform mapHolder;
-    [Range(0, 1)] public float outlinePercent; //地图板块间隔
+    [Range(0, 1)] public float outlinePercent  = 0.1f; //地图板块间隔
     // Start is called before the first frame update
     void Start() {
-        mapSize = new Vector2(8, 8);
-        outlinePercent = 0.1f;
         tilePrefab = Resources.Load<GameObject>("Prefabs/Tile");
         mapHolder = transform.Find("MapHolder").transform;
         GenerateMap();
@@ -24,8 +22,9 @@ public class MapGenerator : MonoBehaviour {
     void GenerateMap() {
         for (int i = 0; i < mapSize.x; i++) {
             for (int j = 0; j < mapSize.y; j++) {
-                Vector3 newPos = new Vector3(-mapSize.x / 2 + 0.5f + i+outlinePercent*i, 0,-mapSize.y / 2 + 0.5f + j+ outlinePercent*j); //计算位置
+                Vector3 newPos = new Vector3(-mapSize.x / 2 + 0.5f + i, 0,-mapSize.y / 2 + 0.5f + j); //计算位置
                 GameObject tile = GameObject.Instantiate(tilePrefab, newPos, Quaternion.Euler(90,0,0), mapHolder);
+                tile.transform.localScale = Vector3.one * (1 - outlinePercent); //间隔效果
             }
         }
     }
