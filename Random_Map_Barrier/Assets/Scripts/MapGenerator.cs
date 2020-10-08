cf317c170e318bf271b45024f77f5f2bc6e13df8
KIPKIPS <2432719656@qq.com>
2020-10-08 21:57:37 +0800

    随机生成障碍物位置完成
    存在随机相同位置的问题,使用洗牌算法解决

--- Random_Map_Barrier/Assets/Scripts/MapGenerator.cs
+++ Random_Map_Barrier/Assets/Scripts/MapGenerator.cs
@@ -7,9 +7,28 @@ public class MapGenerator : MonoBehaviour {
     public Vector2 mapSize = new Vector2(8, 8);
     public Transform mapHolder;
     [Range(0, 1)] public float outlinePercent  = 0.1f; //地图板块间隔
+    public List<Coord> tileCoordsList = new List<Coord>();
+
+    //障碍物预制体
+    public GameObject obsPrefab;
+    public float obsCount;
+
+    [System.Serializable]
+    public struct Coord {
+        public int x;
+        public int y;
+        public Transform trs;
+        public Coord(int x,int y,Transform self) {
+            this.x = x;
+            this.y = y;
+            this.trs = self;
+        }
+    }
     // Start is called before the first frame update
     void Start() {
-        tilePrefab = Resources.Load<GameObject>("Prefabs/Tile");
+        obsPrefab = Resources.Load<GameObject>("Prefabs/Obstacle");//障碍物
+        obsCount = 10;
+        tilePrefab = Resources.Load<GameObject>("Prefabs/Tile");//地图瓦片
         mapHolder = transform.Find("MapHolder").transform;
         GenerateMap();
     }
@@ -26,7 +45,15 @@ public class MapGenerator : MonoBehaviour {
                 Vector3 newPos = new Vector3(-mapSize.x / 2 + 0.5f + i, 0,-mapSize.y / 2 + 0.5f + j); //计算位置
                 GameObject tile = GameObject.Instantiate(tilePrefab, newPos, Quaternion.Euler(90,0,0), mapHolder);
                 tile.transform.localScale = Vector3.one * (1 - outlinePercent); //间隔效果
+
+                tileCoordsList.Add(new Coord(i,j, tile.transform));
             }
+        }
+        //随机障碍生成
+        for (int i = 0; i < obsCount; i++) {
+            Coord randomCoord = tileCoordsList[UnityEngine.Random.Range(0, tileCoordsList.Count)];
+            Vector3 newPos = new Vector3(-mapSize.x / 2 + 0.5f + randomCoord.x, 0.6f, -mapSize.y / 2 + 0.5f + randomCoord.y); //计算位置
+            GameObject obs = GameObject.Instantiate(obsPrefab, newPos, Quaternion.Euler(90, 0, 0), randomCoord.trs);
         }
     }
 }
