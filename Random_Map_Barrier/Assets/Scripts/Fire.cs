dfe0bf0a0ce79328848818d8593dafcf9b457ca2
KIPKIPS <2432719656@qq.com>
2020-10-18 14:37:56 +0800

    增添武器控制模块

---
+++ Random_Map_Barrier/Assets/Scripts/Fire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+    public float speed = 10f;
+
+    public void SetSpeed(float newSpeed) {
+        speed = newSpeed;
+    }
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.forward * Time.deltaTime *speed);
+    }
+}
--- Random_Map_Barrier/Assets/Scripts/GunController.cs
+++ Random_Map_Barrier/Assets/Scripts/GunController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour {
-    // Start is called before the first frame update
+    public Gun equippedGun;
+    public Gun defaultGun;
+    public Transform weaponHolder;
+
     void Start() {
-
+        weaponHolder = transform.Find("WeaponHolder");
+        Debug.Log(weaponHolder == null);
+        defaultGun = Resources.Load<GameObject>("Prefabs/Gun").GetComponent<Gun>();
+        if (defaultGun != null) {
+            EquipGun(defaultGun);
+        }
     }
-
-    // Update is called once per frame
-    void Update() {
-
+    //装备武器
+    public void EquipGun(Gun gunToEquip) {
+        //当前角色身上装备有武器,先把旧的武器销毁掉
+        if (equippedGun != null) {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(gunToEquip, weaponHolder.position,weaponHolder.rotation) as Gun;
+        equippedGun.transform.parent = weaponHolder;
     }
 }
