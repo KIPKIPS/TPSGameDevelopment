0ce8d68ac0ae8703a7db09de16fc4d71e3573178
KIPKIPS <2432719656@qq.com>
2020-10-28 01:54:17 +0800

    解决enemy进攻和寻路穿模的问题
    寻路 : 目标位置 - 方向向量 x 半径和
    攻击 : 目标位置 - 方向向量 x  目标半径

--- Random_Map_Barrier/Assets/Scripts/Enemy.cs
+++ Random_Map_Barrier/Assets/Scripts/Enemy.cs
@@ -14,17 +14,26 @@ public class Enemy : LivingEntity {
     public NavMeshAgent pathFinder;//寻路组件
     private Transform target;//寻路目标
     //private int flag;//标志,控制协程只执行一次
-    public float attackDis = 1.5f;//攻击距离
+    public float attackDis = 0.5f;//攻击距离
+    private Material mat;
+    private Color oriColor;
 
     public float timeBetweenAttack = 1;//两次攻击的间隔时间
     private float nextAttackTime = 0;
+
+    private float targetRadius;
+    private float selfRadius;
 
     protected override void Start() {
         base.Start();
         pathFinder = GetComponent<NavMeshAgent>();//寻路组件
         curState = State.Chase;//默认追赶目标
+        mat = GetComponent<Renderer>().material;
+        oriColor = mat.color;
         //flag = 0;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        targetRadius = target.GetComponent<CapsuleCollider>().radius;//获取半径
+        selfRadius = this.GetComponent<CapsuleCollider>().radius;
         StartCoroutine(UpdatePath());//开启寻路协程,防止每一帧都执行寻路
     }
 
@@ -34,7 +43,7 @@ public class Enemy : LivingEntity {
         if (Time.time > nextAttackTime) {
             //Vector3.Distance(target.transform.position,transform.position)
             float sqrtDis = (target.transform.position - transform.position).sqrMagnitude;//与目标距离的平方
-            if (sqrtDis < Mathf.Pow(attackDis, 2)) {
+            if (sqrtDis < Mathf.Pow(attackDis +selfRadius+targetRadius, 2)) {
                 nextAttackTime = Time.time + timeBetweenAttack;//为下一次攻击时间赋值,now + 攻击间隔
                 //开启攻击协程
                 StartCoroutine(Attack());
@@ -51,14 +60,19 @@ public class Enemy : LivingEntity {
         Vector3 tarPos = target.transform.position;
         float percent = 0;
         while (percent <= 1) {
+            mat.color = Color.black;
+
             percent += Time.deltaTime * attackSpeed;
+            Vector3 dirToTarget = (tarPos - oriPos).normalized;
             float parabola = (-percent * percent + percent) * 4; //抛物线方程 y=-4X² + 4X
-            transform.position = Vector3.Lerp(oriPos,tarPos,parabola); //插值,攻击敌人后又会返回初始位置
+            transform.position = Vector3.Lerp(oriPos,tarPos - dirToTarget*targetRadius, parabola); //插值,攻击敌人后又会返回初始位置
             yield return null;
         }
         //回到初始位置后开启寻路
         pathFinder.enabled = true;
         curState = State.Chase;
+
+        mat.color = oriColor;
     }
 
     //节省性能,在协程中执行寻路,不必在每一帧都去计算路径
@@ -66,7 +80,9 @@ public class Enemy : LivingEntity {
         float refreshRate = 0.25f;
         while (target != null) {
             if (curState == State.Chase) {
-                Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+                Vector3 dirToTarget = (target.position - transform.position).normalized;//计算目标和自身的方向向量,由自身指向目标
+                //防止穿模,接触到target时停下来
+                Vector3 targetPosition = target.position - dirToTarget * (selfRadius + targetRadius + attackDis/2);//目标位置减去方向向量乘半径和
                 if (!dead) {
                     pathFinder.SetDestination(targetPosition);//寻路,会重算寻路路径
                 }
